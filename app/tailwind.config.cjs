35e810a97ee84719d605090eee59802d2630ff76
lphaap <lassi.p.haapala@gmail.com>
2023-06-26 01:30:36 +0300

    major-structure-overhaul-v1

--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -5,6 +5,11 @@ const config = {
     extend: {
       gridTemplateRows: {
         '10': 'repeat(10, minmax(0, 1fr))',
+      },
+      colors: {
+        'lphaap-light-grey': '#1E2435',
+        'lphaap-dark-grey': '#A6ADBB',
+        'lphaap-dark-blue': '#050A18'
       }
     }
   },
